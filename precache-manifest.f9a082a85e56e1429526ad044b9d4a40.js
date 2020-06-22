d9a13c5ae1073df38850562b093a0f061d8e220c
idelduca <idelduca@2u.com>
2020-06-22 11:24:55 -0400

    Updates

---
+++ precache-manifest.f9a082a85e56e1429526ad044b9d4a40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec3da8fb7c7f5dd2453fda2c0a53592",
+    "url": "/sitdown/index.html"
+  },
+  {
+    "revision": "a861f46ceec2d56050af",
+    "url": "/sitdown/static/css/main.781acbb4.chunk.css"
+  },
+  {
+    "revision": "8f2f183733fc53454f8b",
+    "url": "/sitdown/static/js/2.3df61084.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sitdown/static/js/2.3df61084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a861f46ceec2d56050af",
+    "url": "/sitdown/static/js/main.c6377fd9.chunk.js"
+  },
+  {
+    "revision": "86020b54375a593aef9d",
+    "url": "/sitdown/static/js/runtime-main.f63d94bf.js"
+  }
+]);
